3747644ce997faf159f7185d06f8b00c1c8acf09
Grixxx <gleb.grigorev.140@mail.ru>
2023-02-24 15:15:52 +0300

    php blog

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array(
+	'title' => 'Блог о еде',
+	'vk_url' => 'http://vk.com/glebg69',
+	'db' => array(
+		'server' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'name' => 'test_blog'
+	)
+
+);
+
+require "db.php";
---
+++ pr.php
@@ -0,0 +1,12 @@
+<?php
+require "includes/config.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $config['title'] ?></title>
+</head>
+<body>
+</body>
+</html>
